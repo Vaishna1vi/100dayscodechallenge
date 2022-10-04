b7f9aaaf0c1f4d93ef88ecefc1e099c8c6a4b451
Vaishna1vi <114764512+Vaishna1vi@users.noreply.github.com>
2022-10-04 23:03:15 +0530

    Create Day 1.java
    Take 2 inputs from the user and add them.

---
+++ Day 1.java
@@ -0,0 +1,18 @@
+// # 100-Days-code-challenge
+
+import java.util.*;
+class Sum {
+    public static void main(String[] args) {
+        
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the number 1:");
+      int num1 = sc.nextInt();
+      System.out.println("Enter the number 2:");
+      int num2 = sc.nextInt();
+      System.out.println("number 1 = " +num1);
+      System.out.println("number 2 = " +num2);
+      
+      int Sum = num1+num2;
+        System.out.println("Sum of 2 numbers:" +Sum);
+    }
+}
