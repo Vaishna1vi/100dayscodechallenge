b343a0e13f192ca759a330c41565b55e418878d6
Vaishnavi Nagvanshi <114764512+Vaishna1vi@users.noreply.github.com>
2022-10-11 13:04:29 +0530

    Type conversion of datatypes

--- Day 2/Conversion.java
+++ Day 2/Conversion.java
@@ -1,4 +1,18 @@
-# 100dayscodechallenge
+/*  in the given problem we convert the datatype of given character value and performing maths operation on it.
+
+Sample input :
+        char d = 'd'
+        char g = 'g'
+Sample Output :
+            Character value of d : d
+            Character value of g : g
+            Integer value of d : 100
+            Integer value of g : 103
+            Added value of d and g : 203
+            Subtracted value of g and d : 3
+            Multiplicated value of d and g : 10300
+            Divided value of g and d : 1       */
+
 
  public class Conversion {
     public static void main(String[] args) {
