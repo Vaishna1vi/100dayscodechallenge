c0d39e6c890d52fac2baa5eff8a666aa4286c706
Vaishnavi Nagvanshi <114764512+Vaishna1vi@users.noreply.github.com>
2022-10-11 12:48:24 +0530

    Sum of 2 numbers by taking input

--- Day 1/Sum.java
+++ Day 1/Sum.java
@@ -1,4 +1,11 @@
-// # 100-Days-code-challenge
+/* Taking 2 input from the user number 1 and number 2 print the sum of it.
+
+Sample input :
+Enter the number 1: 17
+Enter the number 2: 44
+
+Sample Output :
+Sum of 2 numbers : 61  */
 
 import java.util.*;
 class Sum {
