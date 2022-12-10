233a2d547510b21b597bac249c76a0256b9ae0be
Vaishnavi Nagvanshi <114764512+Vaishna1vi@users.noreply.github.com>
2022-12-10 23:19:42 +0530

    The FoodFactory class contains a method getFood(String) that returns a new instance of Pizza or Cake according to its parameter.

---
+++ Day 54/Java Factory.java
@@ -0,0 +1,96 @@
+/*   In this problem, We have given an interface Food. There are two classes Pizza and Cake which implement the Food interface, and they both contain a method getType().
+
+The main function in the Main class creates an instance of the FoodFactory class. The FoodFactory class contains a method getFood(String) that returns a new instance of Pizza or Cake according to its parameter.
+
+Sample Input 1 :-
+
+cake
+
+Sample Output 1 :-
+
+The factory returned class Cake
+Someone ordered a Dessert!
+
+Sample Input 2 :-
+
+pizza
+
+Sample Output 2 :-
+
+The factory returned class Pizza
+Someone ordered Fast Food!                                      */
+
+
+
+
+import java.util.*;
+import java.security.*;
+interface Food {
+	 public String getType();
+	}
+	class Pizza implements Food {
+	 public String getType() {
+	 return "Someone ordered a Fast Food!";
+	 }
+	}
+
+	class Cake implements Food {
+
+	 public String getType() {
+	 return "Someone ordered a Dessert!";
+	 }
+	}
+	class FoodFactory {
+		public Food getFood(String order) {
+switch (order){
+     case "pizza": return new Pizza();
+     case "cake" : return new Cake();
+     default : return null;
+}
+}//End of getFood method
+
+	}//End of factory class
+
+	public class Solution {
+
+	 public static void main(String args[]){
+			Do_Not_Terminate.forbidExit();
+
+		try{
+
+			Scanner sc=new Scanner(System.in);
+			//creating the factory
+			FoodFactory foodFactory = new FoodFactory();
+	
+			//factory instantiates an object
+			Food food = foodFactory.getFood(sc.nextLine());
+	
+			
+			System.out.println("The factory returned "+food.getClass());
+			System.out.println(food.getType());
+		}
+		catch (Do_Not_Terminate.ExitTrappedException e) {
+			System.out.println("Unsuccessful Termination!!");
+		}
+	 }
+
+	}
+	class Do_Not_Terminate {
+		 
+	    public static class ExitTrappedException extends SecurityException {
+
+			private static final long serialVersionUID = 1L;
+	    }
+	 
+	    public static void forbidExit() {
+	        final SecurityManager securityManager = new SecurityManager() {
+	            @Override
+	            public void checkPermission(Permission permission) {
+	                if (permission.getName().contains("exitVM")) {
+	                    throw new ExitTrappedException();
+	                }
+	            }
+	        };
+	        System.setSecurityManager(securityManager);
+	    }
+	}	                                                                                                                                                                                         
