da7f5b19f475991b8cdf1e64cbf530fa9dc0e3f8
Vaishnavi Nagvanshi <114764512+Vaishna1vi@users.noreply.github.com>
2022-12-12 19:50:00 +0530

    Create Covariant Return Types.java

---
+++ Day 56/Covariant Return Types.java
@@ -0,0 +1,100 @@
+/*  Input Format :-
+
+The locked code reads a single string denoting the name of a subclass of State (i.e., WestBengal, Karnataka, or AndhraPradesh), then tests the methods associated with that subclass.
+
+Output Format :-
+
+Output is handled by the locked code, which creates the object corresponding to the input string's class name and then prints the name returned by that class' national flower's whatsYourName method.
+
+Sample Input :-
+
+AndhraPradesh
+
+Sample Output :-
+
+Lily
+
+Explanation :-
+
+An AndhraPradesh object's yourNationalFlower method returns an instance of the Lily class, and the Lily class' whatsYourName method returns Lily.      */
+
+
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Flower {
+    String whatsYourName() {
+        return "I have many names and types.";
+    }
+}
+
+class Jasmine extends Flower {
+    @Override
+    String whatsYourName() {
+        return "Jasmine";
+    }
+}
+
+class Lily extends Flower {
+    @Override
+    String whatsYourName() {
+        return "Lily";
+    }
+}
+
+class Lotus extends Flower {
+    @Override
+    String whatsYourName() {
+        return "Lotus";
+    }
+}
+
+class Region {
+    Flower yourNationalFlower() {
+        return new Flower();
+    }
+}
+
+class WestBengal extends Region {
+    @Override
+    Jasmine yourNationalFlower() {
+        return new Jasmine();
+    }
+}
+
+class Karnataka extends Region {
+    @Override
+    Lotus yourNationalFlower() {
+        return new Lotus();
+    }
+}
+
+class AndhraPradesh extends Region {
+    @Override
+    Lily yourNationalFlower() {
+        return new Lily();
+    }
+}
+
+
+public class Solution {
+  public static void main(String[] args) throws IOException {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      String s = reader.readLine().trim();
+      Region region = null;
+      switch (s) {
+        case "WestBengal":
+          region = new WestBengal();
+          break;
+        case "AndhraPradesh":
+          region = new AndhraPradesh();
+          break;
+      }
+      Flower flower = region.yourNationalFlower();
+      System.out.println(flower.whatsYourName());
+    }
+}
